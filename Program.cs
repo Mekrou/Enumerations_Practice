466bc1234682a2d5e3195559831bc0c75f073a02
Mekrou <mek.rmills@gmail.com>
2021-01-30 09:52:14 -0500

    added actual enum & used it

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿//
+// Just enum practice...
+// 
+// Created by Mekrou 01/30/2021
+//
+
+using System;
 
 namespace Enumerations
 {
     class Program
     {
+
+        enum DaysOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Wrote from scratch!");
+            DaysOfWeek today; // Declaring our own custom made variable from our enum!
+            today = DaysOfWeek.Saturday;
+            
+            Console.WriteLine("Today is: " + today);
         }
     }
 }
